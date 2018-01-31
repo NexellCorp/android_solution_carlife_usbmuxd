6e9e7b5f4647c13fa6b74ef4b042814700860124
Sungwoo Park <swpark@nexell.co.kr>
2018-01-31 15:15:28 +0900

    nexell: Modify base_config_dir
    This patch modifies base_config_dir because "/var/lib" directory
    is not valid in android system.
    
    Change-Id: I029cb5f8cfcc5edcf5af1a15495fb0b6dafe9c3e
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- src/conf.c
+++ src/conf.c
@@ -125,6 +125,8 @@ const char *config_get_config_dir()
 #else
 #ifdef __APPLE__
 	base_config_dir = strdup("/var/db");
+#elif defined(ANDROID)
+	base_config_dir = strdup("/data/misc/carlife/usbmuxd");
 #else
 	base_config_dir = strdup("/var/lib");
 #endif
